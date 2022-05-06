505f064dde6f55895d99e8e848a61881bb3a8196
Kibo1999 <andregaming5@gmail.com>
2022-05-06 11:09:28 +0100

    read initial state

--- src/mummymaze/MummyMazeAgent.java
+++ src/mummymaze/MummyMazeAgent.java
@@ -1,8 +1,10 @@
 package mummymaze;
 
 import agent.Agent;
+import eightpuzzle.EightPuzzleState;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,9 +23,17 @@ public class MummyMazeAgent extends Agent<MummyMazeState> {
         // teste 3
     }
 
-    public static MummyMazeState readInitialStateFromFile(File file) {
+    public static MummyMazeState readInitialStateFromFile(File file) throws FileNotFoundException {
+        java.util.Scanner scanner = new java.util.Scanner(file);
 
-        return null;
+        char[][] matrix = new char [13][13];
+
+        for (int i = 0; i < 13; i++) {
+            matrix[i] = scanner.nextLine().toCharArray();
+        }
+        initialEnvironment = new MummyMazeState(matrix);
+        resetEnvironment();
+        return environment;
     }
 
     public List<String> getTurns() {
